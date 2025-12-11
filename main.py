64da0a2e42f1e53934d8cc973ed9c555ad8109fb
jkocemba <jason.kocemba@gmail.com>
2025-12-11 11:23:33 +0000

    building out the AI agent

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from bootagentai!")
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise ValueError("API key is required")
+
+def main():
+    parser = argparse.ArgumentParser(description="bootagentai")
+    parser.add_argument("user_prompt", type=str, help="user prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    client = genai.Client(api_key=api_key)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(response.text)
 
 if __name__ == "__main__":
     main()
