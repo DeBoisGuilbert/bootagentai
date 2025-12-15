24cefdace03705055f808713d91f79fe39e20ee6
jkocemba <jason.kocemba@gmail.com>
2025-12-15 15:45:07 +0000

    executing functions llm - finish up the agent to go!

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import os
 import argparse
+from http.client import responses
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from call_function import available_functions, call_function
 from prompts import system_prompt
-from functions.get_files_info import schema_get_files_info
-from functions.write_file_content import schema_write_file
-from functions.run_python_file import schema_run_python_file
-from functions.get_file_content import schema_get_file_content
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise ValueError("API key is required")
 
-available_functions = types.Tool(
-    function_declarations=[schema_get_files_info, schema_run_python_file, schema_write_file, schema_get_file_content],
-)
+
 def main():
     parser = argparse.ArgumentParser(description="bootagentai")
     parser.add_argument("user_prompt", type=str, help="user prompt")
@@ -37,16 +34,32 @@ def main():
         ),
     )
 
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+        return
+
+    function_responses = []
     for function_call in response.function_calls:
-        if function_call:
-            print(f"Calling function: {function_call.name}({function_call.args})")
-        else:
-            print(response.text)
+        function_call_result = call_function(function_call, args.verbose)
+        if (not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if args.verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting")
 
 if __name__ == "__main__":
     main()
